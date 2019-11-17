b06f6ff819accfdbdcd23cfc2a48c89b73a5ff63
xiaoyur347 <xiaoyur347@gmail.com>
2019-11-17 23:35:43 +0800

    update push.py

--- push.py
+++ push.py
@@ -9,11 +9,13 @@ def run_command(cmd):
     p = subprocess.Popen(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     curline = p.stdout.readline()
 
-    while(curline != ""):
-        print (curline)
+    while curline != "":
+        curline = curline.strip()
+        if len(curline) != 0:
+            print (curline)
         curline = p.stdout.readline()
     p.wait()
-    print (p.returncode)
+    print ("ret={}".format(p.returncode))
 
 
 def push(src, dst):
@@ -31,4 +33,4 @@ if __name__ == "__main__":
     push("app/build/intermediates/cmake/debug/obj/{}/test_executable".format(abi), "/data/local/tmp/test_executable")
     push("app/build/intermediates/cmake/debug/obj/{}/libtest_library.so".format(abi), "/data/local/tmp/libtest_library.so")
     shell("chmod 0777 /data/local/tmp/test_executable")
-    # shell("sh -c 'LD_LIBRARY_PATH=/data/local/tmp/ /data/local/tmp/test_executable'")
+    shell("'export LD_LIBRARY_PATH=/data/local/tmp/;/data/local/tmp/test_executable'")
